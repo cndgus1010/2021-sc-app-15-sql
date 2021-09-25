00c3a163abef0e504c72dfa452501ba5db1209c5
cndggus1010 <chung1838@namver.com>
2021-09-25 14:04:20 +0900

    일단 해봐

---
+++ models/auth/index.js
@@ -0,0 +1,5 @@
+// findUser, verifyData, createUser, updateUser, deleteUser
+const createUser = require('./create-user')
+const findUser = require('./find-user')
+
+module.exports = { createUser, findUser }
