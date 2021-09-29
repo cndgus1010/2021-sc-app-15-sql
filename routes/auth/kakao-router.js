f48f531e987e98faa36c0f948ff8b10d4dec1eb8
cndggus1010 <chung1838@namver.com>
2021-09-29 11:19:22 +0900

    dkddkddkdkdkdd

---
+++ routes/auth/kakao-router.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const { error } = require('../../modules/util')
+// const {  } = require('../../models/auth')
+
+router.get('/', passport.authenticate('kakao', { failureRedirect: '/' }), (req, res, next) => {
+	res.redirect('/')
+})
+
+router.get('/cb', (req, res, next) => {
+
+})
+
+module.exports = router
