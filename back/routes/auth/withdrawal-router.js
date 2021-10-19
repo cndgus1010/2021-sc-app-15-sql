4a7cd750b48d32fa4405877f9fbad18d5a5297c9
cndggus1010 <chung1838@namver.com>
2021-10-19 11:43:57 +0900

    탈퇴 처리중

--- back/routes/auth/withdrawal-router.js
+++ back/routes/auth/withdrawal-router.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 const createError = require('http-errors')
 const { alert } = require('../../modules/util')
-const {  } = require('../../models/auth')
+const { deleteUser } = require('../../models/auth')
 const { isUser, isGuest } = require('../../middlewares/auth-mw')
 
 router.get('/', isUser, (req, res, next) => {
@@ -15,12 +15,18 @@ router.get('/', isUser, (req, res, next) => {
 	res.status(200).render('auth/withdrawal')
 })
 
-router.post('/', async (req, res, next) => {
-	// 실제 join 처리
+router.post('/', isUser, async (req, res, next) => {
+	// 실제 탈퇴 처리
 	try {
-		const r = await createUser(req.body)
-		if(r) res.redirect('/')
-		else res.send(alert(ERROR.SQL_ERROR))
+    const { ALERT, ERROR } = req.app.locals;
+    const user = { ...req.body, idx: req.user.idx }
+		const { success } = await deleteUser(user)
+    if(success) {
+			
+			
+			res.send(alert(ALERT.WITHDRAWAL))
+		}
+    else res.send(alert(ERROR.SQL_ERROR))
 	}
 	catch(err) {
 		next(createError(err))
